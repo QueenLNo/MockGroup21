bf92fccec27373d2a21771e18357246f225a5b47
Anuj <Anuj@LAPTOP-FTRQM368>
2023-03-05 19:59:58 +0530

    updated class

--- FB_123/src/test/java/testpack123/java123.java
+++ FB_123/src/test/java/testpack123/java123.java
@@ -3,8 +3,10 @@ package testpack123;
 public class java123 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("hello");
+		System.out.println("Bye");
+		System.out.println("goodday");
+		System.out.println("sleepwell");
 	}
 
 }
